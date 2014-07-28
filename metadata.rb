512b77543eb9f1dffa714749fd9e1eb11a9e8929
Greg Palmier <gregpalmier@gmail.com>
2014-07-28 16:41:19 -0500

    fail.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 name             'gregwants'
 maintainer       'gregpalmier'
 maintainer_email 'gregpalmier@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures gregwants'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
-depends	'apt'
+version          '0.3.0'
+depends		 'apt'
