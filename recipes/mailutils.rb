2fad954e000d64fc9906b753d3c012bb77239908
Greg Palmier <gregpalmier@gmail.com>
2014-07-28 16:30:27 -0500

    mailutils 'n such

---
+++ recipes/mailutils.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: gregwants
+# Recipe:: mailutils
+#
+# Copyright (C) 2014 gregpalmier
+#
+# All rights reserved - Redistribute
+#
+
+case node['platform_family']
+when 'rhel', 'fedora'
+  package 'bind-utils' do
+    action :install
+  end
+when 'debian'
+  package 'mailutils' do
+    action :install
+  end
+end
